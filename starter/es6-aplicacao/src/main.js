4f6f56c5c84aec515f79822fc56e1943bf4b0bfa
MorenaNobre <morenagnobre@gmail.com>
2020-09-12 09:27:04 -0300

    ES6-application-finished

--- starter/es6-aplicacao/src/main.js
+++ starter/es6-aplicacao/src/main.js
@@ -1,69 +1,98 @@
-import api from './api'
+import api from "./api";
 
 class App {
   constructor() {
-    this.repositories = []
+    this.repositories = [];
 
-    this.formEl = document.getElementById('repo-form')
-    this.inputEl = document.querySelector('input[name=repository]')
-    this.listEl = document.getElementById('repo-list')
+    this.formEl = document.getElementById("repo-form");
+    this.inputEl = document.querySelector("input[name=repository]");
+    this.listEl = document.getElementById("repo-list");
 
-    this.registerHandlers()
+    this.registerHandlers();
   }
 
   //registra os eventos
   registerHandlers() {
-    this.formEl.onsubmit = event => this.addRepository(event)
+    this.formEl.onsubmit = (event) => this.addRepository(event);
+  }
+
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span')
+      loadingEl.appendChild(document.createTextNode('Carregando...'))
+      loadingEl.setAttribute('id', 'loading')
+
+      this.formEl.appendChild(loadingEl)
+    } else {
+      document.getElementById('loading').remove()
+    }
   }
 
   async addRepository(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const repoInput = this.inputEl.value
+    const repoInput = this.inputEl.value;
 
-    if (repoInput.length === 0)
-      return
+    if (repoInput.length === 0) return;
 
-    const response = await api.get(`/repos/${repoInput}`)
+    this.setLoading()
 
-    const { name, description, html_url, avatar_url} = response
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-    })
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url },
+      } = response.data;
 
-    this.render()
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
+
+      this.inputEl.value = "";
+
+      this.render();
+    } catch (err) {
+      alert('O repositório não existe!')
+    }
+
+    this.setLoading(false)
+
+    this.inputEl.value = ''
   }
 
   render() {
-    this.listEl.innerHTML = ''
+    this.listEl.innerHTML = "";
 
-    this.repositories.forEach(repo => {
-      let imgEl = document.createElement('img')
-      imgEl.setAttribute('src', repo.avatar_url)
+    this.repositories.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
 
-      let titleEl = document.createElement('strong')
-      titleEl.appendChild(document.createTextNode(repo.name))
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
 
-      let descriptionEl = document.createElement('p')
-      descriptionEl.appendChild(document.createTextNode(repo.description))
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
 
-      let linkEl = document.createElement('a')
-      linkEl.setAttribute('target', '_blank')
-      linkEl.appendChild(document.createTextNode('Acessar'))
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
+      linkEl.appendChild(document.createTextNode("Acessar"));
 
-      let listItemEl = document.createElement('li')
-      listItemEl.appendChild(imgEl)
-      listItemEl.appendChild(titleEl)
-      listItemEl.appendChild(descriptionEl)
-      listItemEl.appendChild(linkEl)
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
 
-      this.listEl.appendChild(listItemEl)
-    })
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
-new App()
+new App();

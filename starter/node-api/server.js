a3e0bfdbeaf99e6b3883d5a29768d202638df2b0
MorenaNobre <morenagnobre@gmail.com>
2020-09-19 08:35:42 -0300

    nodejs-done

--- starter/node-api/server.js
+++ starter/node-api/server.js
@@ -1,26 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
 
 
 //iniciando app
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 //iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 requireDir('./src/models')
 
-const Product = mongoose.model('Product')
+// const Product = mongoose.model('Product')
 
-//primeira rota
-app.get('/', (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  })
-
-  return res.send('Hello world!')
-})
+//Rotas
+app.use('/api', require('./src/routes'))
 
 app.listen(3001)
